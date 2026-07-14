d386c3fe1b757e034a4845a1522ae11ce2c309c1
Wing <114337707+wing-0902@users.noreply.github.com>
2026-07-14 21:22:56 +0900

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,56 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+import 'katex/contrib/mhchem';
+
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: '物理 - dict.digital',
+      htmlAttrs: {
+        lang: 'ja'
+      }
+    }
+  },
+  compatibilityDate: '2025-07-15',
+  content: {
+    build: {
+      markdown: {
+        highlight: {
+          theme: 'night-owl',
+          langs: ['js', 'jsx', 'ts', 'tsx', 'svelte']
+        },
+        remarkPlugins: {
+          'remark-math': {}
+        },
+        rehypePlugins: {
+          'rehype-katex': {}
+        }
+      }
+    }
+  },
+  css: [
+    '@unocss/reset/sanitize/sanitize.css',
+    '@unocss/reset/sanitize/assets.css',
+    '~/assets/global.scss',
+    'katex/dist/katex.min.css'
+  ],
+  devtools: { enabled: true },
+  experimental: {
+    defaults: {
+      nuxtLink: {
+        trailingSlash: 'append'
+      }
+    }
+  },
+  modules: ['@nuxtjs/sitemap', '@nuxt/content', '@unocss/nuxt'],
+  site: {
+    url: 'https://physics.hs.dict.digital',
+    name: '物理 - dict.digital',
+    trailingSlash: true
+  },
+  vite: {
+    optimizeDeps: {
+      include: ['@vue/devtools-core', '@vue/devtools-kit', '@vueuse/core']
+    }
+  }
+});
